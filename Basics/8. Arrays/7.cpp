4e50cc84b81f9fed6a2ad51cbae761d0a546016e
Shahadat Hossain <sht.kcn@gmail.com>
2026-07-09 13:49:09 +0600

    7 & 8 solved

---
+++ Basics/8. Arrays/7.cpp
@@ -0,0 +1,54 @@
+// F. Reversing
+//link: https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/F
+//author: https://codeforces.com/profile/iam-phoenix
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vs = vector<string>;
+using vl = vector<ll>;
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define allr(a) a.rbegin(), a.rend()
+#define sz(x) (int)(x).size()
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define endl '\n'
+
+
+void solve() {
+    
+    int n;
+    cin >> n;
+
+    int mid = (n%2 == 0? n/2 - 1 : n/2);
+    vl arr(n);
+
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    for(int i=0; i<=mid; i++){
+        int temp = arr[n-1-i];
+        arr[n-1-i] = arr[i];
+        arr[i] = temp;
+    }
+
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    solve();
+
+    return 0;
+}
